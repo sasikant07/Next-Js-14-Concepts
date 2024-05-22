1e97eb29a97e65fdf3bc103ece72dc520bb5bd5e
Sasikant Biswal <biswalsashikant@gmail.com>
2024-05-23 02:48:39 +0530

    some changes

--- hello-world/app/page.tsx
+++ hello-world/app/page.tsx
@@ -29,14 +29,7 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <p>Hello World</p>
       </div>
 
       <div className={styles.grid}>
