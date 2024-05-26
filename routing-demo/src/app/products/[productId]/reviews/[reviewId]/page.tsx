15d82c2bfe1fc303e260977e740b6d8d502d8939
Sasikant Biswal <biswalsashikant@gmail.com>
2024-05-27 01:34:51 +0530

    Advanced roting system

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,21 @@
+"use client";
 import { notFound } from "next/navigation";
 
-const ProductDetails = ({
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
+const ReviewDetail = ({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) => {
+  // const random = getRandomInt(2);
+
+  // if (random === 1) {
+  //   throw new Error("Error loading review");
+  // }
+
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
@@ -15,4 +26,4 @@ const ProductDetails = ({
   );
 };
 
-export default ProductDetails;
+export default ReviewDetail;
