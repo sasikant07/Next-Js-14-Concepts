7830866ac10c0b5249a003c8964d5b49f230317d
Sasikant Biswal <biswalsashikant@gmail.com>
2024-06-02 02:41:40 +0530

    Added route handlers mredirect, query, cookies request and middlewares

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -1,7 +1,14 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export async function GET() {
-  return Response.json(comments);
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+
+  return Response.json(filteredComments);
 }
 
 export async function POST(request: Request) {
@@ -18,4 +25,3 @@ export async function POST(request: Request) {
     status: 201,
   });
 }
-
