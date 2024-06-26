21bf16e5b2648e15f8075027fcd43933d827b604
Sasikant Biswal <biswalsashikant@gmail.com>
2024-06-27 02:12:37 +0530

    static & dynamic rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,5 +1,10 @@
+import { cookies } from "next/headers";
 export default function AboutPage() {
+  const cookieStore = cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme);
+
   console.log("About server component");
 
-  return <h1>About Page</h1>;
+  return <h1>About Page {new Date().toLocaleTimeString()}</h1>;
 }
